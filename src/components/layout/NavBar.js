06084f312b93cc753e2af46aa04947da1e96629d
Filip Jamalov <filip.jamalov@endava.com>
2021-09-10 09:57:18 +0200

    final project pokedex blue

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+
+class NavBar extends Component {
+    render() { 
+        return ( 
+            <div>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+                    <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">
+                        Blue Pokedex
+                    </a>
+                </nav>
+            </div>
+         );
+    }
+}
+ 
+export default NavBar;
